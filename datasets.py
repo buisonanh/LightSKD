623224d6fbda5153f6c821d18582bb84cbb96a91
Son Anh <buisonanh.study@gmail.com>
2024-09-12 13:17:34 +0700

    update

--- datasets.py
+++ datasets.py
@@ -61,7 +61,7 @@ def get_fer_transforms():
     target_size = 48
     mean, std = 0.5, 0.5
     transform_train = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
+        #transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
         transforms.RandomResizedCrop(target_size, scale=(0.8, 1.2)),
         transforms.RandomApply([transforms.RandomAffine(0, translate=(0.2, 0.2))], p=0.5),
         transforms.RandomHorizontalFlip(),
@@ -70,7 +70,7 @@ def get_fer_transforms():
         transforms.Normalize(mean=mean, std=std)
     ])
     transform_test = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
+        #transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
         transforms.Resize((target_size, target_size)),
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std)
